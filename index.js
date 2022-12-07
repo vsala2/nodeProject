d1cdc2534b35ca521e29f459da2b7645724e0f93
vsala2 <98728821+vsala2@users.noreply.github.com>
2022-12-06 20:26:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -58,18 +58,26 @@ const server = http.createServer(function (req, res) {
   }
 });
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+  "Access-Control-Max-Age": 2592000, // 30 days
+  /** add other headers too */
+};
+
+
 server.listen(PORT);
 console.log('Server running at {{PORT}}');
 
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "https://quaint-ray-woolens.cyclic.app/api",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
-        },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
+// exports.handler = async (event) => {
+//     const response = {
+//         statusCode: 200,
+//         headers: {
+//             "Access-Control-Allow-Headers" : "Content-Type",
+//             "Access-Control-Allow-Origin": "https://quaint-ray-woolens.cyclic.app/api",
+//             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+//         },
+//         body: JSON.stringify('Hello from Lambda!'),
+//     };
+//     return response;
+// };

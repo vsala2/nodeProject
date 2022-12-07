e1da65bf2e1f203d864d8c0c868df7ac3016f1e3
vsala2 <98728821+vsala2@users.noreply.github.com>
2022-12-06 20:20:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -60,3 +60,16 @@ const server = http.createServer(function (req, res) {
 
 server.listen(PORT);
 console.log('Server running at {{PORT}}');
+
+exports.handler = async (event) => {
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "https://quaint-ray-woolens.cyclic.app/api",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
